7d1ccfdac2607aa0336e19bd6d9adb097db622ca
Marco Satti <marcosatti@gmail.com>
2013-07-27 14:17:53 +0800

    cleanup, organised

--- src/com/github/marco9999/directtalk/ConnectMenu.java
+++ src/com/github/marco9999/directtalk/ConnectMenu.java
@@ -1,8 +1,8 @@
 package com.github.marco9999.directtalk;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
@@ -24,14 +24,18 @@ public class ConnectMenu extends Activity
 		getMenuInflater().inflate(R.menu.connect_menu, menu);
 		return true;
 	}
-	
-	public void connect_onClick(View view) 
+
+	public void connect_onClick(View view)
 	{
 		Intent intent = new Intent(this, MessageWindow.class);
 		EditText host = (EditText) findViewById(R.id.connect_menu_hosttext);
 		EditText port = (EditText) findViewById(R.id.connect_menu_porttext);
 		String hoststring = host.getText().toString();
 		String portstring = port.getText().toString();
+		if (portstring.isEmpty())
+		{
+			portstring = "4444";
+		}
 		intent.putExtra("com.github.marco9999.hoststring", hoststring);
 		intent.putExtra("com.github.marco9999.portstring", portstring);
 		startActivity(intent);
--- src/com/github/marco9999/directtalk/MessageHandlerWorker.java
+++ src/com/github/marco9999/directtalk/MessageHandlerWorker.java
@@ -4,72 +4,98 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
+
 import android.util.Log;
 
 public class MessageHandlerWorker extends Thread
 {
-	String _ip;
-	String _port;
-	
-	public MessageHandlerWorker(String ip, String port)
+	String ip;
+	String port;
+	Socket connection;
+
+	public MessageHandlerWorker(String _ip, String _port)
 	{
-		_ip = ip;
-		_port = port;
+		ip = _ip;
+		port = _port;
+		connection = null;
 	}
+
+	@Override
 	public void run()
 	{
-		InetAddress ip = null;
-		int port = 0;
-		
-		try
-		{
-			ip = InetAddress.getByName(_ip);
-			port = Integer.parseInt(_port);
-		} 
-		catch (Exception e)
-		{
-			Log.e("DirectTalk", "Error: Host or port invalid!");
-			e.printStackTrace();
-		}
-		
-		InputStream input = null;
-		Socket connection = null;
-		int ch = 0;
+		// Create socket to communicate
+		createMessageSocket();
+
+		// Read byte and log it
+		readByteFromSocketStream();
+	}
+
+	public void createMessageSocket()
+	{
+		InetAddress v_ip = null;
+		int v_port = 0;
 
 		try
 		{
-			connection = new Socket(ip, port);
-		} 
+			v_ip = InetAddress.getByName(ip);
+			v_port = Integer.parseInt(port);
+		}
+		catch (UnknownHostException e)
+		{
+			Log.e("DirectTalk", "Error: Unknown host!");
+			e.printStackTrace();
+			return;
+		}
+		catch (NumberFormatException e)
+		{
+			Log.e("DirectTalk", "Error: Invalid port!");
+			e.printStackTrace();
+			return;
+		}
+
+		try
+		{
+			connection = new Socket(v_ip, v_port);
+		}
 		catch (IOException e)
 		{
-			Log.e("DirectTalk", "Error: connection failed!");
+			Log.e("DirectTalk", "Error: I/O connection failed!");
 			e.printStackTrace();
-			System.exit(1);
+			return;
 		}
+	}
 
+	public void readByteFromSocketStream()
+	{
+		InputStream input = null;
+		int ch = 0;
+
+		// Get input stream
 		try
 		{
 			input = connection.getInputStream();
-		} 
+		}
 		catch (IOException e)
 		{
 			Log.e("DirectTalk", "Error: couldn't get input stream!");
 			e.printStackTrace();
-			System.exit(1);
+			return;
 		}
-		
+
+		// Read byte
 		try
 		{
 			while ((ch = input.read()) != -1)
 			{
 				Log.i("DirectTalk", "Read a byte: " + ch);
 			}
-		} 
+		}
 		catch (IOException e)
 		{
 			Log.e("DirectTalk", "Error: couldn't read stream!");
 			e.printStackTrace();
-			System.exit(1);
+			return;
 		}
 	}
 }
